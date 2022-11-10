1ab780bca3100601cb60ae541aec705459d55286
Josh Chappelow <jdchappelow@gmail.com>
2022-11-10 16:33:45 +0900

    feat: useeffect button animation when items are changed

--- src/Components/Layout/HeaderCheckoutButton.jsx
+++ src/Components/Layout/HeaderCheckoutButton.jsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext , useEffect, useState} from 'react';
 import CheckoutContext from '../../Store/checkout-context';
 import styles from './HeaderCheckoutButton.module.css'
 
@@ -6,12 +6,35 @@ import CartIcon from '../Checkout/CheckoutIcon';
 
 
 function HeaderCheckoutButton(props) {
+  const [btnHighlight, setBtnHighlight] = useState(false)
+
   const cartCtx = useContext(CheckoutContext)
+
   const itemCount = cartCtx.items.reduce((currentNumber, item) => {
     return currentNumber + item.amount
   }, 0)
+
+  const { items } = cartCtx
+
+  const btnClasses = `${styles.button} ${btnHighlight ? styles.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnHighlight(true)
+
+    const timer = setTimeout(() => {
+      setBtnHighlight(false)
+    }, 300);
+
+    return () => {
+      clearTimeout(timer)
+    }
+  }, [items])
+
   return (
-    <button className={styles.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={styles.icon}>
         <CartIcon/>
       </span>
