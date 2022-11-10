7f2a83b5ca3844c0912a6215fda01243472563ae
Josh Chappelow <jdchappelow@gmail.com>
2022-11-10 16:23:11 +0900

    feat: plus minus buttons working in checkout

--- src/Components/Checkout/Cart.jsx
+++ src/Components/Checkout/Cart.jsx
@@ -1,17 +1,33 @@
 import React, { useContext } from "react";
 import Modal from "../UI/Modal";
+import CartItem from "./CartItem";
 import styles from "./Cart.module.css";
 import CheckoutContext from "../../Store/checkout-context";
 
 function Cart(props) {
-const checkoutctx = useContext(CheckoutContext)
-const totalPrice = `$${checkoutctx.totalAmount.toFixed(2)}`
-const hasItems = checkoutctx.items.length > 0
+  const checkoutctx = useContext(CheckoutContext);
+  const totalPrice = `$${checkoutctx.totalAmount.toFixed(2)}`;
+  const hasItems = checkoutctx.items.length > 0;
+
+  function checkoutItemRemoveHandler(id) {
+    checkoutctx.removeItem(id)
+  }
+
+  function checkoutItemAddHandler(item) {
+    checkoutctx.addItem({...item, amount: 1})
+  }
 
   const cartItems = (
     <ul className={styles["cart-items"]}>
       {checkoutctx.items.map((item) => (
-        <li>{item.name}</li>
+        <CartItem
+          key={item.id}
+          name={item.name}
+          amount={item.amount}
+          price={item.price}
+          onRemove={checkoutItemRemoveHandler.bind(null, item.id)}
+          onAdd={checkoutItemAddHandler.bind(null, item)}
+        />
       ))}
     </ul>
   );
@@ -24,9 +40,11 @@ const hasItems = checkoutctx.items.length > 0
         <span>{totalPrice}</span>
       </div>
       <div className={styles.actions}>
-        {hasItems && <button className={styles.button} onClick={props.onCloseCheckout}>
-          Order
-        </button>}
+        {hasItems && (
+          <button className={styles.button} onClick={props.onCloseCheckout}>
+            Order
+          </button>
+        )}
         <button
           className={styles["button--alt"]}
           onClick={props.onCloseCheckout}
--- src/Store/CheckoutProvider.jsx
+++ src/Store/CheckoutProvider.jsx
@@ -1,41 +1,79 @@
-import React from 'react';
-import CheckoutContext from './checkout-context';
-import {useReducer} from 'react'
+import React from "react";
+import CheckoutContext from "./checkout-context";
+import { useReducer } from "react";
 
 const defaultCheckout = {
   items: [],
-  totalAmount: 0
-}
+  totalAmount: 0,
+};
 
 const checkoutReducer = (state, action) => {
   if (action.type === "ADD") {
-    const updatedItems = state.items.concat(action.item);
-    const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount
+    const updatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    const existingIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCheckoutItem = state.items[existingIndex];
+    let updatedItems;
+
+    if (existingCheckoutItem) {
+      const updatedItem = {
+        ...existingCheckoutItem,
+        amount: existingCheckoutItem.amount + action.item.amount,
+      };
+      updatedItems = [...state.items];
+      updatedItems[existingIndex] = updatedItem
+    } else {
+      updatedItems = state.items.concat(action.item);
+    }
+    return {
+      items: updatedItems,
+      totalAmount: updatedTotalAmount,
+    }
+  }
+  if (action.type === 'REMOVE') {
+    const existingIndex = state.items.findIndex(
+      (item) => item.id === action.id
+    );
+    const existingCheckoutItem = state.items[existingIndex]
+    const updatedTotalAmount = state.totalAmount - existingCheckoutItem.price
+    let updatedItems;
+    if (existingCheckoutItem.amount === 1) {
+      updatedItems = state.items.filter(item => item.id !== action.id)
+    }else {
+      const updatedItem = {...existingCheckoutItem, amount: existingCheckoutItem.amount - 1}
+      updatedItems = [...state.items]
+      updatedItems[existingIndex] = updatedItem
+    }
+
     return {
       items: updatedItems,
       totalAmount: updatedTotalAmount
     }
   }
-  return defaultCheckout
-}
+  return defaultCheckout;
+};
 
 function CheckoutProvider(props) {
-  const [checkoutState, dispatchCheckoutAction] = useReducer(checkoutReducer, defaultCheckout)
+  const [checkoutState, dispatchCheckoutAction] = useReducer(
+    checkoutReducer,
+    defaultCheckout
+  );
 
-  const addItemToCOHandler = item => {
-    dispatchCheckoutAction({type: 'ADD', item: item})
-  }
-  const removeItemFromCOHandler = ID => {
-    dispatchCheckoutAction({type: "REMOVE", ID: ID})
-  }
-
+  const addItemToCOHandler = (item) => {
+    dispatchCheckoutAction({ type: "ADD", item: item });
+  };
+  const removeItemFromCOHandler = (ID) => {
+    dispatchCheckoutAction({ type: "REMOVE", id: ID });
+  };
 
   const checkoutContext = {
     items: checkoutState.items,
     totalAmount: checkoutState.totalAmount,
     addItem: addItemToCOHandler,
-    removeItem: removeItemFromCOHandler
-  }
+    removeItem: removeItemFromCOHandler,
+  };
 
   return (
     <CheckoutContext.Provider value={checkoutContext}>
